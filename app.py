1d4317f74da82db12af4947a840303b7dcf516bf
LucianoNeo <tec.info.luciano@hotmail.com>
2022-10-21 17:33:20 -0300

    get game by id endpoint

--- app.py
+++ app.py
@@ -21,9 +21,21 @@ games = [
 ]
 
 
-@app.route('/games')
+@app.route('/', methods=['GET'])
+def start():
+    return jsonify({"welcome": "to Neo's Favorite Games Python API"})
+
+
+@app.route('/games', methods=['GET'])
 def get_games():
     return jsonify(games)
+
+
+@app.route('/games/<int:id>', methods=['GET'])
+def get_game_by_id(id):
+    for game in games:
+        if game.get('id') == id:
+            return jsonify(game)
 
 
 app.run(port=5000, host='localhost', debug=True)
